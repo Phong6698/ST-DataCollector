5ef2e99b5db50fe9698c65a8762280ccda5fb839
Phong6698 <Phong@hotmail.ch>
2016-04-26 20:47:33 +0200

    Stuff

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 	$sec = "1500";
 	$summonerId = "";
 	$api_key = "58453580-a12b-497a-bdde-d1255bd0fda3";
+	$servername = "localhost:3307";
+	$username = "root";
+	$password = "";
+	$dbname = "st-datacollector";
 // 	1500 = 25 min
 // 	summonerid = 67540676
 ?>
@@ -11,6 +15,7 @@
 <head>
 	<meta charset="utf-8">	
 	<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+	<script src="JavaScript.js"></script>
 	<title>Summoner Tracker - Data Collector</title>
 </head>
 <body>
@@ -20,48 +25,64 @@
 	</div>
 	<hr />
 	<div class="content">
-		<?php 
+		<div id="countdown">
 		
-			$servername = "localhost:3307";
-			$username = "root";
-			$password = "";
-			$dbname = "st-datacollector";
-			
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			// Check connection
-			if ($conn->connect_error) {
-			    die("Connection failed: " . $conn->connect_error);
-			}
-			echo "Connected successfully";
-			
-			$sql = "SELECT * FROM summoner";
-			$result = $conn->query($sql);
-			if (!$result) {
-				throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
-			}
-			if ($result->num_rows > 0) {
-				// output data of each row
-				while($row = $result->fetch_assoc()) {
-					echo "<h1>" . $row["SummonerId"]. "</h1>";		
-					$summonerId = $row["SummonerId"];
-					$url = "https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/" . $summonerId . "/recent?api_key=" . $api_key;
-						
-					$resultJSON = file_get_contents($url);
-					$resultJSON_decoded = json_decode($resultJSON);
-						
-						
-					foreach($resultJSON_decoded->games as $game){
-						echo $game->gameMode." ".$game->gameType." ".$game->subType;
-						echo "<br>";
-					}
-				}
-			} else {
-				echo "0 results";
-			}
-			
-			$conn->close();
-			
+		</div>
+		<?php
+			echo 'Last Update: '. date("H:i:s d.m.Y") ."\n";			
 		?>
+		<progress id="progress" value="10" max="100"></progress>
+
+		<div class="summoners">
+			<?php 
+			
+				
+				
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+				echo "Connected successfully";
+				
+				$sql = "SELECT * FROM summoner";
+				$result = $conn->query($sql);
+				if (!$result) {
+					throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
+				}
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) {
+						echo '<div class="summoner">';
+						echo "<h2>" . $row["SummonerId"]. "</h2>";		
+						$summonerId = $row["SummonerId"];
+						$urlapi = "https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/" . $summonerId . "/recent?api_key=" . $api_key;
+						$urllocal = "http://localhost:8080/ST-DataCollector/LocalJSON.json";	
+						$resultJSON = file_get_contents($urlapi);
+						$resultJSON_decoded = json_decode($resultJSON);
+							
+							
+						foreach($resultJSON_decoded->games as $game){
+							echo $game->gameMode." ".$game->gameType." ".$game->subType;
+							echo "<br>";
+						}
+						echo '</div>';
+					}
+				} else {
+					echo "0 results";
+				}
+				
+				$conn->close();
+				
+			?>
+		</div>
+		<br>
+		
+		<script type="text/javascript">
+		  var timeSec = <?php echo $sec; ?>; 
+		  var display = document.querySelector('#countdown');
+		  startTimer(timeSec, display);
+		</script>
 	</div>
 	
 </body>
